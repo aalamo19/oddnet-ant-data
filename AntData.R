ecf741275d2adc2d43605365b016342c40a4b3ea
Andreína Alamo <106687496+aalamo19@users.noreply.github.com>
2024-07-25 15:20:28 -0500

    upload project

---
+++ AntData.R
@@ -0,0 +1,32 @@
+library(igraph)
+library(dplyr)
+library(oddnet)
+library(tsibble)
+
+#dat <- read_graph("~/../Desktop/DatosRedes/insecta-ant-colony5.edges")
+
+dat2 <- read.table("data/insecta-ant-colony5.edges")
+
+
+
+
+# datos originales --------------------------------------------------------
+
+
+trescol <- dat2 |> select(-V3) |> dplyr::rename(day = V4)
+#Datos originales, pero agrupados
+ant41_red_df <- trescol |> group_by(day)
+# Separados por dias (41)
+ant41_red_list <-  group_split(ant41_red_df)
+
+networks <- list()
+for (i in 1:length(ant41_red_list)) {
+  gr <- graph_from_data_frame(ant41_red_list[[i]])
+  networks[[i]] <- as_adjacency_matrix(gr)
+  
+}
+
+anom <- anomalous_networks(networks)
+anom
+
+
